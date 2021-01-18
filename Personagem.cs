b5a556badf3da17d1cf8a753a775bc3837895d0a
unknown <leandroiay3@gmail.com>
2021-01-18 01:20:27 -0300

    Objetos e Classes

---
+++ Personagem.cs
@@ -0,0 +1,22 @@
+namespace Objeto_Classe
+{
+    public class Personagem    
+        { 
+ 
+        public string nome = "Tony Stark";
+
+        public int idade = 40;
+
+        public string armadura;
+
+        public string Atacar(){
+            return "Ataque de fogo!";
+        }
+
+        public string Defender(){
+            return "Não foi dessa vez!";           
+           }
+
+    }
+
+}
---
+++ Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace Objeto_Classe
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            
+            Personagem objPersonagem = new Personagem();
+
+            objPersonagem.nome = "Tsukamoto";
+            objPersonagem.idade = 32;
+            objPersonagem.armadura = "Voadora";
+
+            Personagem objPersonagem2 = new Personagem();
+
+            objPersonagem2.nome = "Joyce";
+            objPersonagem2.idade = 18;
+            objPersonagem2.armadura = "Flash";
+
+            Console.WriteLine($"Personagem 1 = {objPersonagem.nome}");
+            Console.WriteLine($"Personagem 2 = {objPersonagem2.nome}"); 
+            Console.WriteLine($"Ataque = {objPersonagem.Atacar()}");
+            Console.WriteLine($"Defesa = {objPersonagem2.Defender()}"); 
+        }
+    }
+}
